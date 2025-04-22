d178269d21555c2890afc6a13bfb718ed27d4549
aswdevaraj <aswin.devaraj@freshworks.com>
2025-04-22 09:00:44 +0530

    restructured homepage

--- src/main/java/pages/Homepage.java
+++ src/main/java/pages/Homepage.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import locators.Elements;
 import locators.Widgets;
-
+import locators.Alertsclass;
 public class Homepage {
 	private WebDriver driver1;
 	public Wait<WebDriver> waits;
@@ -22,38 +22,30 @@ public class Homepage {
 	    driver1=driver;
 		this.waits=wait;
 	}
-    private By alerticon= By.xpath("//h5[contains(text(),'Alerts, Frame & Windows')]");
-    private By alertleftmenu= By.xpath("//span[text()='Alerts']");
-    private By firstclickme = By.id("alertButton");
-    private By timeralert = By.id("timerAlertButton");
-    private By confirmbutton= By.id("confirmButton");
-    private By promptbutton = By.id("promtButton");
-    private By frameleftmenu = By.xpath("(//span[contains(text(),'Frames')])[1]");
-    private By biggerframelocator = By.id("sampleHeading");
-    		//This method will handle all types of alert and as part of learning this is the first topic
+    //This method will handle all types of alert and as part of learning this is the first topic
     public void alerthandling(){
-    	WebElement alerticon1=driver1.findElement(alerticon);
+    	WebElement alerticon1=driver1.findElement(Alertsclass.alerticon);
     	waits.until(d ->alerticon1.isDisplayed());
     	alerticon1.click();    	
-    	WebElement alertleftmenu1=driver1.findElement(alertleftmenu);
+    	WebElement alertleftmenu1=driver1.findElement(Alertsclass.alertleftmenu);
     	waits.until(d ->alertleftmenu1.isDisplayed());
     	alertleftmenu1.click();
-    	WebElement firstclickme1=driver1.findElement(firstclickme);
+    	WebElement firstclickme1=driver1.findElement(Alertsclass.firstclickme);
     	waits.until(d ->firstclickme1.isDisplayed());
     	firstclickme1.click();
     	String text=driver1.switchTo().alert().getText();
     	System.out.println(text);
     	driver1.switchTo().alert().accept();
-    	WebElement timeralert1 = driver1.findElement(timeralert);
+    	WebElement timeralert1 = driver1.findElement(Alertsclass.timeralert);
     	timeralert1.click();
     	waits.until(ExpectedConditions.alertIsPresent()); //for alert to present in screen- we need to use explicit wait and no implicit wait will work here
     	System.out.println(driver1.switchTo().alert().getText());
     	driver1.switchTo().alert().accept();
-    	WebElement confirmbutton1 = driver1.findElement(confirmbutton);
+    	WebElement confirmbutton1 = driver1.findElement(Alertsclass.confirmbutton);
     	confirmbutton1.click();
     	System.out.println(driver1.switchTo().alert().getText());
     	driver1.switchTo().alert().accept();
-    	WebElement promptbox1 = driver1.findElement(promptbutton);
+    	WebElement promptbox1 = driver1.findElement(Alertsclass.promptbutton);
     	waits.until(d ->promptbox1.isDisplayed());
     	promptbox1.click();
     	waits.until(ExpectedConditions.alertIsPresent());
@@ -63,19 +55,19 @@ public class Homepage {
     	alert.accept();
     }
     public void switchingframes() {
-    	WebElement alerticon1=driver1.findElement(alerticon);
+    	WebElement alerticon1=driver1.findElement(Alertsclass.alerticon);
     	waits.until(d ->alerticon1.isDisplayed());
     	alerticon1.click();
-    	WebElement frameleftmenu1=driver1.findElement(frameleftmenu);
+    	WebElement frameleftmenu1=driver1.findElement(Alertsclass.frameleftmenu);
     	waits.until(d ->frameleftmenu1.isDisplayed());
     	frameleftmenu1.click();
     	driver1.switchTo().frame("frame1");
-    	WebElement bigframe1=driver1.findElement(biggerframelocator);
+    	WebElement bigframe1=driver1.findElement(Alertsclass.biggerframelocator);
     	System.out.println(bigframe1.getText());
     	// we need to switch to default before switching to another frame
     	driver1.switchTo().defaultContent();
     	driver1.switchTo().frame("frame2");
-    	WebElement bigframe2=driver1.findElement(biggerframelocator);
+    	WebElement bigframe2=driver1.findElement(Alertsclass.biggerframelocator);
     	System.out.println(bigframe2.getText());
     	driver1.switchTo().defaultContent();
     }
